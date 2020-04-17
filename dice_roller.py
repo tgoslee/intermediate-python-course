d77fc6e4919406a7eb14af5bf207b10e53362d3d
tgoslee <60710670+tgoslee@users.noreply.github.com>
2020-04-17 10:17:55 -0400

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum = dice_sum + roll
-    print(f'You rolled a {roll}')
+    if roll ==1:
+      print(f'You rolled a {roll}! Fail!')
+    elif roll == 6:
+      print(f'You rolled a {roll}! Lit!')
+    else:
+      print(f'You rolled a {roll}')
   print(f'You have rolled a total of {dice_sum}')
 if __name__== "__main__":
   main()
